14272c40ddb66aa3733a7d08ee3312248222132a
castleodinland <castleodinland@gmail.com>
2015-03-11 13:38:56 +0800

    test html fetch
    test html fetch

--- flaskapp.py
+++ flaskapp.py
@@ -6,8 +6,6 @@ from flask import Flask, request, flash, url_for, redirect, \
 import urllib2
 import thread
 from threading import Timer
-import smtplib,re  
-from email.mime.text import MIMEText  
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
@@ -15,35 +13,20 @@ app.config.from_pyfile('flaskapp.cfg')
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<strong>Hello, Maomaosong!!!</strong>"
 
+    return "<strong>Hello, world!</strong>"
 @app.route("/test")
 def test():
-    return "<strong>It's Alive!</strong>"
+    f = urllib2.urlopen('http://www.windeln.de/zh/aptamil-milchnahrung.html?selectedean=4008976022299')
+    return "%s" %(f.read())
 
 def canSendMailAgain():
     print "Timer handler"
-    
-sourc_mail_addr = "cexuscastle@126.com"
-dest_mail_addr = ["cexuscastle@126.com"]
-
-def sendWarningMail (text):  
-    msg = MIMEText(text)  
-    msg['Subject'] = "Aptamil 1+ comming!!!"  
-    msg['From'] = sourc_mail_addr  
-    smtp = smtplib.SMTP()  
-    smtp.connect(r'smtp.126.com')
-    smtp.login(sourc_mail_addr, "zxcvbnm123")  
-    smtp.sendmail(sourc_mail_addr, dest_mail_addr, msg.as_string())  
-    smtp.close() 
     
 if __name__ == '__main__':
     app.debug = True
     #TIMERRUNNING = Timer(2.0, canSendMailAgain, [])
     #TIMERRUNNING.start()
-    #f = urllib2.urlopen('http://www.python.org/')
-    #print f.read(100)
-    sendWarningMail("hahaha")
     app.run()
 
     
