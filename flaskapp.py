6c1e3ddd7b791b31f519e1bec3da64eb96ad3886
castleodinland <castleodinland@gmail.com>
2015-03-11 10:00:01 +0800

    tst send email
    tst send email

--- flaskapp.py
+++ flaskapp.py
@@ -3,6 +3,12 @@ from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
 
+import urllib2
+import thread
+from threading import Timer
+import smtplib,re  
+from email.mime.text import MIMEText  
+
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
@@ -15,5 +21,30 @@ def index():
 def test():
     return "<strong>It's Alive!</strong>"
 
+def canSendMailAgain():
+    print "Timer handler"
+    
+sourc_mail_addr = "cexuscastle@126.com"
+dest_mail_addr = ["cexuscastle@126.com"]
+
+def sendWarningMail (text):  
+    msg = MIMEText(text)  
+    msg['Subject'] = "Aptamil 1+ comming!!!"  
+    msg['From'] = sourc_mail_addr  
+    smtp = smtplib.SMTP()  
+    smtp.connect(r'smtp.126.com')
+    smtp.login(sourc_mail_addr, "zxcvbnm123")  
+    smtp.sendmail(sourc_mail_addr, dest_mail_addr, msg.as_string())  
+    smtp.close() 
+    
 if __name__ == '__main__':
+    app.debug = True
+    #TIMERRUNNING = Timer(2.0, canSendMailAgain, [])
+    #TIMERRUNNING.start()
+    #f = urllib2.urlopen('http://www.python.org/')
+    #print f.read(100)
+    sendWarningMail("hahaha")
     app.run()
+
+    
+
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='FlaskApp',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='A basic Flask app',
+      author='Castle Odinland,
+      author_email='castle_odinland@126.com',
       url='http://www.python.org/sigs/distutils-sig/',
      install_requires=['Flask>=0.10.1'],
      )
